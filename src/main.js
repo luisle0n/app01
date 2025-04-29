8843a78045a398765242302bf6b24ba8e28e70c5
luisle0n <lfleon9@utpl.edu.ec>
2025-04-29 09:35:27 -0500

    Proyecto Vite con jQuery y DataTables

--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import $ from 'jquery';
 import dt from 'datatables.net-dt';
 
 $(document).ready(function () {
-  $('#tabla-ejemplo').DataTable();
+  const mi_tabla = $('#tabla-ejemplo').DataTable();
+
+  const url = "https://pkgstore.datahub.io/core/continent-codes/continent-codes_json/data/60d6baef1250bc2b01fd0148dcccc518/continent-codes_json.json";
+
+  $("#obtenerInformacion").click(function () {
+    $.ajax({
+      dataType: 'json',
+      url: url
+    }).then(function (data) {
+      data.forEach(function (item) {
+        mi_tabla.row.add([
+          item.Code,
+          item.Name
+        ]).draw(false);
+      });
+    });
+  });
+
+  $("#limpiarInformacion").click(function () {
+    mi_tabla.clear().draw();
+  });
 });
